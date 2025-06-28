683c2be72da1f341bbdc0fc59d210dfa66ea3347
Zer08Bytes <41458106+Zer08Bytes@users.noreply.github.com>
2025-06-28 09:40:21 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["101.43.103.154"];
+pub const RS_PUB_KEY: &str = "Zvv4c73oqDUu0tMsCdxRsggH5hOveYtlRE5KIhpeguM=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
